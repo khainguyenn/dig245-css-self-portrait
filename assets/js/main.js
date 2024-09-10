08fab362d18b22785e69e65252729b033b25b844
Khai Nguyen <khnguyen@davidson.edu>
2024-09-10 16:00:44 -0400

    completed html, css, js self portrait

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,21 @@ Staaatliches Bauhaus (1919-1933)
 `;
 
 console.log(bauhaus);
+
+document.addEventListener("mousemove", (event) => {
+  let mousex = event.clientX;
+  let mousey = event.clientY;
+  document.getElementById("mouse").innerHTML = "X = " + mousex + ", " + "Y = " + mousey;
+});
+
+
+const allElements = document.querySelectorAll('*'); 
+console.log("Total number of HTML elements in the portrait:", allElements.length);
+
+document.getElementById("colorButton").addEventListener("click", function() {
+  if (document.body.style.backgroundColor === "red") {
+      document.body.style.backgroundColor = "white";
+  } else {
+      document.body.style.backgroundColor = "red";
+  }
+});
